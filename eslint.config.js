f70f86c98b53a7351de20fc3222970dc57e10df3
iqbalalayubbi <iqbal.is415@gmail.com>
2024-11-23 16:54:59 +0700

    chore: initialize project setup with prettier, ESLint, and commit conventional

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      'coverage',
+      '**/public',
+      '**/dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+  eslintPluginPrettier,
+];
---
+++ packages/common/dist/common.js
@@ -0,0 +1,7 @@
+const n = (e) => e == null, s = (e) => e && typeof e == "object", o = (e) => n(e) || Array.isArray(e) && e.length === 0 || s(e) && Object.keys(e).length === 0 || typeof e == "string" && e.trim().length === 0, i = "iqbal";
+export {
+  o as isBlank,
+  n as isEmpty,
+  s as isObject,
+  i as name
+};
