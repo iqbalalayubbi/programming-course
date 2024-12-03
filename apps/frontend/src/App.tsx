c2cf21ff32b765ddcd8efc13e7c05cafe7db474c
iqbalalayubbi <iqbal.is415@gmail.com>
2024-12-03 08:12:42 +0700

    refactor: remove unused file

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,15 +1,5 @@
-import { isBlank, name } from 'common';
-
 const App = () => {
-  return (
-    <>
-      <p>undefined isBlank - {isBlank(undefined) ? 'true' : 'false'}</p>
-      <p>false isBlank - {isBlank(false) ? 'true' : 'false'}</p>
-      <p>true isBlank - {isBlank(true) ? 'true' : 'false'}</p>
-      <p>Empty object isBlank - {isBlank({}) ? 'true' : 'false'}</p>
-      <h1>{name}</h1>
-    </>
-  );
+  return <h1>hello world</h1>;
 };
 
 export default App;
