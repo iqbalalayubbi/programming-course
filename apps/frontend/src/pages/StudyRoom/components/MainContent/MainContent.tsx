6c05e68ce25ea69d85aa50afc13c21c7d216d01c
iqbalalayubbi <iqbal.is415@gmail.com>
2024-12-10 09:24:11 +0700

    feat: add title, image, and tabs content at study room page

--- apps/frontend/src/pages/StudyRoom/components/MainContent/MainContent.tsx
+++ apps/frontend/src/pages/StudyRoom/components/MainContent/MainContent.tsx
@@ -1,5 +1,38 @@
+import { thumbnailCourse } from '@/assets';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Button, Flex } from 'antd';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+
 const MainContent = () => {
-  return <div>MainContent</div>;
+  const items: TabsProps['items'] = [
+    {
+      key: '1',
+      label: 'Contents',
+      children: 'Content of Tab Pane 1',
+    },
+    {
+      key: '2',
+      label: 'Task',
+      children: 'Content of Tab Pane 2',
+    },
+    {
+      key: '3',
+      label: 'Discussion',
+      children: 'Content of Tab Pane 3',
+    },
+  ];
+
+  return (
+    <Flex gap={8} vertical>
+      <Flex align="center" gap={16}>
+        <Button type="text" icon={<ArrowLeftOutlined />} />
+        <h1 className="font-bold text-4xl my-5">Basic HTML</h1>
+      </Flex>
+      <img src={thumbnailCourse} alt="" className="w-full" />
+      <Tabs defaultActiveKey="1" items={items} className="my-5" />
+    </Flex>
+  );
 };
 
 export { MainContent };
